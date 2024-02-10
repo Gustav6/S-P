aa9c1d0fe562523e029cf26b7508d89d1b46fde6
Tramshy <simon.hansen1000@gmail.com>
2024-02-10 23:56:16 +0100

    Player shows Invincibility when dashing

--- Assets/Test Folder/Simon/Dash.cs
+++ Assets/Test Folder/Simon/Dash.cs
@@ -1,13 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class Dash : PowerUp
 {
     private PlayerMovement _player;
     private Rigidbody2D _rb;
-
     private BoxCollider2D _playerHitbox;
+
+    private List<SpriteRenderer> _sr;
+    private Color[] _initialColor;
+
+    private Color _invincibleColor = new Color(0.6f, 0.7f, 0.7f);
 
     private float _dashTime = 0.5f;
 
@@ -15,8 +20,12 @@ public class Dash : PowerUp
     {
         _player = GetComponent<PlayerMovement>();
         _rb = GetComponent<Rigidbody2D>();
-
         _playerHitbox = GetComponent<BoxCollider2D>();
+
+        _sr = GetComponentsInChildren<SpriteRenderer>().ToList();
+        _sr.RemoveAt(4); // 4 = Weapon flash sprite.
+
+        _initialColor = new Color[_sr.Count];
     }
 
     public override void UsePowerUp()
@@ -32,12 +41,26 @@ public class Dash : PowerUp
         StartCoroutine(MakePlayerInvincible());
     }
 
-    // TODO: Add a little something to show invincibility.
     private IEnumerator MakePlayerInvincible()
     {
         _playerHitbox.enabled = false;
 
+        int rendererIndex = 0;
+
+        foreach (SpriteRenderer sr in _sr)
+        {
+            _initialColor[rendererIndex] = sr.color;
+            rendererIndex++;
+
+            sr.color = _invincibleColor;
+        }
+
         yield return new WaitForSeconds(_dashTime);
+
+        for (int i = 0; i < rendererIndex; i++)
+        {
+            _sr[i].color = _initialColor[i];
+        }
 
         _playerHitbox.enabled = true;
         PlayerStats.Instance.ClearEquippedAbility();

91513cbd737957fdc5cd4d9fa4924d3abbaea2f6
hgkarmic <tristan.marriott@elev.ga.lbs.se>
2024-02-12 14:42:51 +0100

    Tree
    smaller updates

--- Assets/Test Folder/Tristan/Scripts/TileDestroyer.cs
+++ Assets/Test Folder/Tristan/Scripts/TileDestroyer.cs
@@ -39,10 +39,10 @@ public class TileDestroyer : MonoBehaviour
         snappedTile = new Vector3Int(tilePos.x,tilePos.y,0);
 
         
-        if (Input.GetMouseButtonUp(0))
+        /*if (Input.GetMouseButtonUp(0))
         {
             DestroyTile(mousePos, 50);
-        }
+        }*/
     }
 
     Tile_Type GetTile_Type(Tile tile) {
@@ -85,7 +85,7 @@ public class TileDestroyer : MonoBehaviour
     }
 
     void UpdateTile(Tile_Location loc) {
-        // Checks all possible cases and places the correct tile for said case
+        // Checks (almost) all possible cases and places the correct tile for said case
         switch (loc)
         {
             case Tile_Location.N:
